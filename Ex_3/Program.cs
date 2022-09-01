de1c69aecdbf51127378bdd6922ff233145ca6fa
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-09-01 14:03:56 +0300

    factorial/commit

---
+++ Ex_3/Program.cs
@@ -0,0 +1,11 @@
+﻿double Factorial(int n)
+{
+    // 1!=1
+    // 0!=1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 0; i < 16; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
