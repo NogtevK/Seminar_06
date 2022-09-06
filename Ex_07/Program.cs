cd53da1a646926df9a1f5963953d2630703d74a4
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-09-07 01:32:24 +0300

    2nd solution/testOK/commit and push

--- Ex_07/Program.cs
+++ Ex_07/Program.cs
@@ -4,16 +4,39 @@
 Если N = 3 -> 0 1 1
 Если N = 7 -> 0 1 1 2 3 5 8 
 */
-Console.WriteLine("Введите число N: ");
+/* Console.WriteLine("Введите число N: ");
 int N = int.Parse(Console.ReadLine());
 int a = 0;
 int b = 1;
 int Fibonacci = 0;
 Console.Write(a + "  " + b);
-for (int i = 1; i < N-1; i++)
+for (int i = 1; i < N - 1; i++)
 {
     Fibonacci = a + b;
-    Console.Write(" "+Fibonacci+" ");
+    Console.Write(" " + Fibonacci + " ");
     a = b;
     b = Fibonacci;
 }
+ */
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите Число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] mass = new int[N];
+mass[0] = 0;
+mass[1] = 1;
+for (int i = 2; i < mass.Length; i++)
+{
+    mass[i] = mass[i - 2] + mass[i - 1];
+}
+Console.WriteLine();
+Console.WriteLine("Числа Фибоначчи: ");
+Print(mass);
