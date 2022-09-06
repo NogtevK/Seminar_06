8b1f45486a5c18119e8b94be9ede63ec40654f48
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-09-07 02:01:17 +0300

    task 45 complete/TestOk/commit/push

---
+++ Ex_08/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 45: Напишите программу, которая будет создавать
+копию заданного массива с помощью поэлементного
+копирования.  */
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+Console.WriteLine();
+
+int[] mass = new int[5];
+int[] copy = new int[mass.Length];
+for (int i = 0, j = 0; i < mass.Length && j < mass.Length; i++, j++)
+{
+    mass[i] = new Random().Next(0, 10);
+    copy[j] = mass[i];
+
+}
+Print(mass);
+Console.WriteLine();
+Print(copy);
+Console.WriteLine();
+
+
